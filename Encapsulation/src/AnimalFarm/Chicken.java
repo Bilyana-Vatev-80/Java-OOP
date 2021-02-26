4e0681cd77f5507a5ee5078a29de47ed8e3496d7
Bilyana Vatev <bilyanavatev@gmail.com>
2021-02-26 06:36:00 +0000

    homework problem

--- Encapsulation/src/AnimalFarm/Chicken.java
+++ Encapsulation/src/AnimalFarm/Chicken.java
@@ -8,12 +8,33 @@ public class Chicken {
         this.setName(name);
         this.setAge(age);
     }
-
-    public void setName(String name) {
-        this.name = name;
+    private void setName (String name){
+        if(name.length() < 1){
+            throw new IllegalArgumentException("Name cannot be empty.");
+        }
+         this.name = name;
     }
-
-    public void setAge(int age) {
+    private void setAge (int age){
+        if(age < 0 || age > 15) {
+            throw new IllegalArgumentException("Age should be between 0 and 15.");
+        }
         this.age = age;
+    }
+    private double calculateProductPerDay() {
+        if(this.age < 5){
+            return 2;
+        } else if (this.age < 11){
+            return 1;
+        } else {
+            return 0.75;
+        }
+    }
+    public double productPerDay (){
+        return calculateProductPerDay();
+    }
+    @Override
+    public String toString (){
+        return String.format("Chicken %s (age %d) can produce %.2f eggs per day.",
+                this.name,this.age,this.productPerDay() );
     }
 }
