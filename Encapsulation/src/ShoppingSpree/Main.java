9cb346831ec0dbdf2ea404002615d6039336fa88
Bilyana Vatev <bilyanavatev@gmail.com>
2021-02-26 10:30:31 +0000

    homework problem

---
+++ Encapsulation/src/ShoppingSpree/Main.java
@@ -0,0 +1,62 @@
+package ShoppingSpree;
+
+import java.util.Arrays;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Map<String, Person> people;
+        Map<String,Product> products;
+        try {
+            people = setPeople (scanner);
+            products = setProducts (scanner);
+        }catch (IllegalArgumentException ex){
+            System.out.println (ex.getMessage ());return;
+        }
+
+        String command = scanner.nextLine ();
+
+        while (!"END".equals (command)){
+            String[] tokens = command.split ("\\s+");
+
+            people.get (tokens[0]).buyProduct (products.get (tokens[1]));
+
+            command = scanner.nextLine ();
+        }
+
+
+        for (Map.Entry<String, Person> stringPersonEntry : people.entrySet ()) {
+            System.out.println (stringPersonEntry.getValue ());
+        }
+
+
+    }
+
+    private static
+    Map<String, Product> setProducts (Scanner scanner) {
+        Map<String,Product> products = new LinkedHashMap<> ();
+        String[] productsInfo = scanner.nextLine ().split (";");
+        for (int i = 0; i < productsInfo.length; i++) {
+            String[] tokens = productsInfo[i].split ("=");
+            Product product = new Product (tokens[0],Double.parseDouble (tokens[1]) );
+            products.put (tokens[0],product);
+        }
+        return products;
+    }
+
+    private static
+    Map<String, Person> setPeople (Scanner scanner) {
+        Map<String,Person> people = new LinkedHashMap<> ();
+        String[] peopleInfo = scanner.nextLine ().split (";");
+        for (int i = 0; i < peopleInfo.length; i++) {
+            String[] tokens = peopleInfo[i].split ("=");
+            Person person = new Person (tokens[0],Double.parseDouble (tokens[1]) );
+            people.put (tokens[0],person );
+        }
+        return people;
+    }
+
+}
---
+++ Encapsulation/src/ShoppingSpree/Person.java
@@ -0,0 +1,61 @@
+package ShoppingSpree;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Person {
+    private String name;
+    private double money;
+    private List<Product> products;
+
+    public Person (String name,double money){
+        this.setName(name);
+        this.setMoney(money);
+        this.products = new ArrayList<>();
+    }
+
+    private void setMoney(double money) {
+        if(money < 0){
+            throw new IllegalArgumentException("Money cannot be negative");
+        }
+        this.money = money;
+    }
+
+    private void setName(String name) {
+        if(name == null || name.trim().isEmpty()){
+            throw new IllegalArgumentException("Name cannot be empty");
+        }
+        this.name = name;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    @Override
+    public String toString (){
+        StringBuilder sb = new StringBuilder(this.name + " - ");
+        if(this.products.isEmpty()){
+            sb.append("Nothing bought");
+        } else {
+            for (int i = 0; i < products.size(); i++) {
+                if(i < products.size() - 1){
+                    sb.append(products.get(i).getName()).append(", ");
+                } else {
+                    sb.append(products.get(i).getName());
+                }
+            }
+        }
+        return sb.toString();
+    }
+    public void buyProduct (Product product ) {
+     if (money >= product.getCost()){
+         this.products.add(product);
+         this.money -= product.getCost();
+         System.out.printf("%s bought %s%n",this.name,product.getName());
+     } else {
+         System.out.printf("%s can't afford %s%n",this.name,product.getName());
+     }
+    }
+
+}
