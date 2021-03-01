12b03c73a67157b288fad18016d38353a904ceed
Bilyana Vatev <bilyanavatev@gmail.com>
2021-03-01 06:03:58 +0000

    homework problem

--- Interfaces and Abstraction/src/CarShop/Main.java
+++ Interfaces and Abstraction/src/CarShop/Main.java
@@ -2,7 +2,7 @@ package CarShop;
 
 public class Main {
     public static void main(String[] args) {
-        Car seat = new Seat("Leon", "gray", 110, "Spain", 11111.1);
+        Car seat = new Seat ("Leon", "gray", 110, "Spain");
 
         System.out.printf (
                 "%s is %s color and have %s horse power%n",
