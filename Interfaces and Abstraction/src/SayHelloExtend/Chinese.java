8f46bcdac3fe365642abba134af34ea8bb52ff6e
Bilyana Vatev <bilyanavatev@gmail.com>
2021-03-01 10:54:33 +0000

    homework problem

---
+++ Interfaces and Abstraction/src/SayHelloExtend/Chinese.java
@@ -0,0 +1,14 @@
+package SayHelloExtend;
+
+public class Chinese extends BasePerson{
+
+    public Chinese(String name){
+        super(name);
+    }
+
+    @Override
+    public
+    String sayHello () {
+        return "Djydjybydjy";
+    }
+}
